be1e8e0fbc5f8c70c32ffdaa0e614c5dd4fdab68
davidemvf <davidemv.fiore@gmail.com>
2020-10-10 16:30:21 +0200

    Aggiunte transazioni all'header

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  
+  opened: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+      console.log(this.opened);
+      
+  }
+
+
+  toggleNav() {
+      this.opened = !this.opened;
   }
 
 }
